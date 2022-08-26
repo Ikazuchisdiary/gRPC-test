e3a05a98e55448913b701ad60d5ff3224cd661d2
いかずち <ikazuchisdiary@gmail.com>
2022-08-26 05:51:50 +0000

    クライアントコード

---
+++ cmd/client/main.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	greetv1 "example/gen/greet/v1"
+	"example/gen/greet/v1/greetv1connect"
+
+	"github.com/bufbuild/connect-go"
+)
+
+func main() {
+	client := greetv1connect.NewGreetServiceClient(
+		http.DefaultClient,
+		"http://localhost:8080",
+		connect.WithGRPCWeb(),
+	)
+	res, err := client.Greet(
+		context.Background(),
+		connect.NewRequest(&greetv1.GreetRequest{Name: "Jane"}),
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(res.Msg.Greeting)
+}
